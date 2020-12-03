fb2046f1012493e3666e260d884b35dce54867e2
Robert Tanev <rtanev@cleancodefactory.de>
2020-12-03 18:02:51 +0200

    Version 1.0.16 Log only when properly initialized

--- src/KraftLogger.cs
+++ src/KraftLogger.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Hosting;
 namespace Ccf.Ck.Libs.Logging
 {
     public class KraftLogger : Microsoft.Extensions.Logging.ILogger
-    { 
+    {
         private static Logger _Logger;
         private readonly NLogProviderOptions _Options;
         private static string _ContentRootPath = string.Empty;
@@ -45,55 +45,62 @@ namespace Ccf.Ck.Libs.Logging
 
         public static ReadOnlyCollection<Target> GetAllTargets()
         {
-            return LogManager.Configuration.AllTargets;
+            if (_Logger != null)
+            {
+                return LogManager.Configuration.AllTargets;
+            }
+            return null;
         }
 
         //todo  callsite showing the framework logging classes/methods
         public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            var nLogLogLevel = ConvertLogLevel(logLevel);
-            if (!IsEnabled(nLogLogLevel))
+            if (_Logger != null)
             {
-                return;
-            }
-            if (formatter == null)
-            {
-                throw new ArgumentNullException(nameof(formatter));
-            }
-            var message = formatter(state, exception);
-
-            //message arguments are not needed as it is already checked that the loglevel is enabled.
-            var eventInfo = LogEventInfo.Create(nLogLogLevel, _Logger.Name, message);
-            eventInfo.Exception = exception;
-            if (!_Options.IgnoreEmptyEventId || eventId.Id != 0 || !string.IsNullOrEmpty(eventId.Name))
-            {
-                // Attempt to reuse the same string-allocations based on the current <see cref="NLogProviderOptions.EventIdSeparator"/>
-                var eventIdPropertyNames = _EventIdPropertyNames ?? new Tuple<string, string, string>(null, null, null);
-                var eventIdSeparator = _Options.EventIdSeparator ?? string.Empty;
-                if (!ReferenceEquals(eventIdPropertyNames.Item1, eventIdSeparator))
+                var nLogLogLevel = ConvertLogLevel(logLevel);
+                if (!IsEnabled(nLogLogLevel))
                 {
-                    // Perform atomic cache update of the string-allocations matching the current separator
-                    eventIdPropertyNames = new Tuple<string, string, string>(
-                        eventIdSeparator,
-                        string.Concat("EventId", eventIdSeparator, "Id"),
-                        string.Concat("EventId", eventIdSeparator, "Name"));
-                    _EventIdPropertyNames = eventIdPropertyNames;
+                    return;
                 }
-
-                var idIsZero = eventId.Id == 0;
-                eventInfo.Properties[eventIdPropertyNames.Item2] = idIsZero ? ZeroEventId : eventId.Id;
-                eventInfo.Properties[eventIdPropertyNames.Item3] = eventId.Name;
-                eventInfo.Properties["EventId"] = idIsZero && eventId.Name == null ? EmptyEventId : eventId;
-            }
-            if (exception != null)
-            {
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(logLevel, exception));
-                if (_Accessor.HttpContext != null) // you should check HttpContext 
+                if (formatter == null)
                 {
-                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(logLevel, new object[] { _Accessor.HttpContext }));
+                    throw new ArgumentNullException(nameof(formatter));
                 }
+                var message = formatter(state, exception);
+
+                //message arguments are not needed as it is already checked that the loglevel is enabled.
+                var eventInfo = LogEventInfo.Create(nLogLogLevel, _Logger.Name, message);
+                eventInfo.Exception = exception;
+                if (!_Options.IgnoreEmptyEventId || eventId.Id != 0 || !string.IsNullOrEmpty(eventId.Name))
+                {
+                    // Attempt to reuse the same string-allocations based on the current <see cref="NLogProviderOptions.EventIdSeparator"/>
+                    var eventIdPropertyNames = _EventIdPropertyNames ?? new Tuple<string, string, string>(null, null, null);
+                    var eventIdSeparator = _Options.EventIdSeparator ?? string.Empty;
+                    if (!ReferenceEquals(eventIdPropertyNames.Item1, eventIdSeparator))
+                    {
+                        // Perform atomic cache update of the string-allocations matching the current separator
+                        eventIdPropertyNames = new Tuple<string, string, string>(
+                            eventIdSeparator,
+                            string.Concat("EventId", eventIdSeparator, "Id"),
+                            string.Concat("EventId", eventIdSeparator, "Name"));
+                        _EventIdPropertyNames = eventIdPropertyNames;
+                    }
+
+                    var idIsZero = eventId.Id == 0;
+                    eventInfo.Properties[eventIdPropertyNames.Item2] = idIsZero ? ZeroEventId : eventId.Id;
+                    eventInfo.Properties[eventIdPropertyNames.Item3] = eventId.Name;
+                    eventInfo.Properties["EventId"] = idIsZero && eventId.Name == null ? EmptyEventId : eventId;
+                }
+                if (exception != null)
+                {
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(logLevel, exception));
+                    if (_Accessor.HttpContext != null) // you should check HttpContext 
+                    {
+                        MappedDiagnosticsContext.Set("Args", Utilities.Serialize(logLevel, new object[] { _Accessor.HttpContext }));
+                    }
+                }
+                _Logger.Log(eventInfo);
             }
-            _Logger.Log(eventInfo);
         }
 
         /// <summary>
@@ -112,7 +119,11 @@ namespace Ccf.Ck.Libs.Logging
         /// </summary>
         private bool IsEnabled(NLog.LogLevel logLevel)
         {
-            return _Logger.IsEnabled(logLevel);
+            if (_Logger != null)
+            {
+                return _Logger.IsEnabled(logLevel);
+            }
+            return false;
         }
 
         /// <summary>
@@ -154,140 +165,179 @@ namespace Ccf.Ck.Libs.Logging
 
         public static void EnableDisableLogging(bool enable)
         {
-            if (enable)
+            if (_Logger != null)
             {
-                LogManager.EnableLogging();
+                if (enable)
+                {
+                    LogManager.EnableLogging();
+                }
+                else
+                {
+                    LogManager.DisableLogging();
+                }
             }
-            else
-            {
-                LogManager.DisableLogging();
-            }          
         }
 
         public static void LogCritical(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, args));
-                _Logger.Log(NLog.LogLevel.Fatal, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, args));
+                    _Logger.Log(NLog.LogLevel.Fatal, message);
+                }).Wait();
+            }
         }
 
         public static void LogCritical(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, args));
-                _Logger.Log(NLog.LogLevel.Fatal, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Critical, args));
+                    _Logger.Log(NLog.LogLevel.Fatal, exception, message);
+                }).Wait();
+            }
         }
 
         public static void LogDebug(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, args));
-                _Logger?.Log(NLog.LogLevel.Debug, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, args));
+                    _Logger?.Log(NLog.LogLevel.Debug, message);
+                }).Wait();
+            }
         }
 
         public static void LogDebug(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, args));
-                _Logger.Log(NLog.LogLevel.Debug, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Debug, args));
+                    _Logger.Log(NLog.LogLevel.Debug, exception, message);
+                }).Wait();
+            }
         }
 
         public static void LogError(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, args));
-                _Logger.Log(NLog.LogLevel.Error, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, args));
+                    _Logger.Log(NLog.LogLevel.Error, message);
+                }).Wait();
+            }
         }
 
         public static void LogError(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, args));
-                _Logger.Log(NLog.LogLevel.Error, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Error, args));
+                    _Logger.Log(NLog.LogLevel.Error, exception, message);
+                }).Wait();
+            }
         }
 
         public static void LogInformation(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, args));
-                _Logger.Log(NLog.LogLevel.Info, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, args));
+                    _Logger.Log(NLog.LogLevel.Info, message);
+                }).Wait();
+            }
         }
 
         public static void LogInformation(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, args));
-                _Logger.Log(NLog.LogLevel.Info, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Information, args));
+                    _Logger.Log(NLog.LogLevel.Info, exception, message);
+                }).Wait();
+            }
         }
 
         public static void LogTrace(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, args));
-                _Logger.Log(NLog.LogLevel.Trace, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, args));
+                    _Logger.Log(NLog.LogLevel.Trace, message);
+                }).Wait();
+            }
         }
 
         public static void LogTrace(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, args));
-                _Logger.Log(NLog.LogLevel.Trace, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Trace, args));
+                    _Logger.Log(NLog.LogLevel.Trace, exception, message);
+                }).Wait();
+            }
         }
 
         public static void LogWarning(string message, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, args));
-                _Logger.Log(NLog.LogLevel.Warn, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, args));
+                    _Logger.Log(NLog.LogLevel.Warn, message);
+                }).Wait();
+            }
         }
 
         public static void LogWarning(Exception exception, string message = null, params object[] args)
         {
-            Task.Factory.StartNew(() =>
+            if (_Logger != null)
             {
-                MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
-                MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, exception));
-                MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, args));
-                _Logger.Log(NLog.LogLevel.Warn, exception, message);
-            }).Wait();
+                Task.Factory.StartNew(() =>
+                {
+                    MappedDiagnosticsContext.Set("ExecutionFolder", _ContentRootPath);
+                    MappedDiagnosticsContext.Set("KraftError", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, exception));
+                    MappedDiagnosticsContext.Set("Args", Utilities.Serialize(Microsoft.Extensions.Logging.LogLevel.Warning, args));
+                    _Logger.Log(NLog.LogLevel.Warn, exception, message);
+                }).Wait();
+            }
         }
         #endregion Static Methods
     }
