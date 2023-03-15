3483a32aa4afc2c9b6bbb71dd020972357a11547
Robert Tanev <rtanev@cleancodefactory.de>
2023-03-15 12:48:03 +0200

    Version 7.0.1  Updated nugets and fixed example nlog.config

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,46 +1,88 @@
 ﻿using Ccf.Ck.Libs.Logging;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace TestLogger
 {
-    class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        private static string[] _Args;
+        private static bool _RestartRequest;
+        private static CancellationTokenSource _CancellationToken = new CancellationTokenSource();
+
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            _Args = args;
+            //_Args = new string[1];
+            //_Args[0] = @"/mnt/d/_Development/Ccf/CcfRepositories/Customers/Website_AndGrains";
+
+            await StartServer();
+            while (_RestartRequest)
+            {
+                _RestartRequest = false;
+                await StartServer();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-               Host.CreateDefaultBuilder(args)
-                   .ConfigureServices((hostContext, services) =>
-                   {
-                       services.UseBindKraftLogger();
-                       ServiceProvider serviceProvider = services.BuildServiceProvider();
-                       ApplicationBuilder builder = new ApplicationBuilder(serviceProvider);
-                       var loggerFactory = builder.ApplicationServices.GetService<ILoggerFactory>();
-                       builder.UseBindKraftLogger(null, loggerFactory, null);
+        public static void Restart(bool restart)
+        {
+            _RestartRequest = restart;
+            if (restart)
+            {
+                KraftLogger.LogInformation($"Restart: executed");
+                Console.WriteLine($"========= Restarting App at: {DateTime.Now.ToLongTimeString()} =========");
+            }
+            _CancellationToken.Cancel();
+        }
 
-                       try
-                       {
-                           try
-                           {
-                               throw new Exception("Inner");
-                           }
-                           catch (Exception inner)
-                           {
-                               throw new Exception("Outer", inner);
-                           }                           
-                       }
-                       catch (Exception ex)
-                       {
-                           KraftLogger.LogError(ex);
-                           Console.WriteLine(ex.Message);
-                           Console.ReadLine();
-                       }
-                   });
+        private static async Task StartServer()
+        {
+            try
+            {
+                _CancellationToken = new CancellationTokenSource();
+                IHostBuilder hostBuilder = CreateHostBuilder(_Args);
+                await hostBuilder.RunConsoleAsync(_CancellationToken.Token);
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            string contentRoot = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            if (args.Length > 0)
+            {
+                contentRoot = Path.GetFullPath(args[0]);
+            }
+            Console.WriteLine("Content root: " + contentRoot);
+            return Host.CreateDefaultBuilder(args)
+                .UseContentRoot(contentRoot)
+                .ConfigureLogging((hostBuilder, logging) =>
+                {
+                    logging.ClearProviders();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    //webBuilder.
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.Limits.MaxConcurrentConnections = null;
+                        serverOptions.Limits.MaxConcurrentUpgradedConnections = null;
+                        serverOptions.Limits.MaxRequestBodySize = null;
+                        serverOptions.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(20));
+                        serverOptions.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(20));
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
     }
 }
---
+++ tests/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Ccf.Ck.Libs.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogger
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.UseBindKraftLogger();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        {
+            ILoggerFactory loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
+            app.UseBindKraftLogger(env, loggerFactory, "/error");
+
+            try
+            {
+                try
+                {
+                    throw new Exception("Inner");
+                }
+                catch (Exception inner)
+                {
+                    throw new Exception("Outer", inner);
+                }
+            }
+            catch (Exception ex)
+            {
+                KraftLogger.LogError(ex);
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
